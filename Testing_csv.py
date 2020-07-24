e19347d8a592bcc5eff5d3bc394bf58b5d23c5f9
rajveersingh <rajveer.rajput.18.1996@gmail.com>
2020-07-24 12:53:57 +0530

    OYD files

--- Testing_csv.py
+++ Testing_csv.py
@@ -1,17 +1,26 @@
 import pandas as pd
 
-inpt_csv = pd.read_csv("/home/superadmin/DBDA/project/OYD/summary.csv")
-
-del inpt_csv['ID']
 
 
-Limit = int(input("Enter Limit value:- "))
+# del inpt_csv['ID']
+# print(inpt_csv)
 
-Min_value = float(input("Enter min double value between 0 and 1:- "))
+class Testing_csv():
+    def __init__(self):
+        self.__inpt_csv = pd.read_csv("/home/superadmin/DBDA/project/OYD/summary.csv", index_col='ID')
+        self._Limit =0
+        self._Min_value=0.0
+    def user_input(self):
+        self._Limit = int(input("Enter Limit value:- "))
 
-for j in inpt_csv.head(Limit):
-    for i in inpt_csv[j].head(Limit):
-        if i> Min_value:
-            result =pd.DataFrame((f"{j}: ",round(i,9)))
-            print(result)
-            result.to_csv("/home/superadmin/DBDA/project/OYD/result.csv",mode='a',index=False)
+        self._Min_value = float(input("Enter min double value between 0 and 1:- "))
+    def csv_writing(self):
+        for j in self.__inpt_csv.head(self._Limit):
+            for i in self.__inpt_csv[j].head(self._Limit):
+                if i> self._Min_value:
+                    result =pd.DataFrame(f"{j}: "+str(round(i,9)))
+                    result.to_csv("/home/superadmin/DBDA/project/OYD/result.csv",mode='a',index=False)
+
+obj =Testing_csv()
+obj.user_input()
+obj.csv_writing()
