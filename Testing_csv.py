6306aa8115edc41980cdd032190818fbe973b239
rajveersingh <rajveer.rajput.18.1996@gmail.com>
2020-07-24 12:15:43 +0530

    OYD files

---
+++ Testing_csv.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+inpt_csv = pd.read_csv("/home/superadmin/DBDA/project/OYD/summary.csv")
+
+
+# print(inpt_csv.head(10))
+
+Limit = int(input("Enter Limit value:- "))
+
+Min_value = float(input("Enter min double value between 0 and 1:- "))
+
+for j in inpt_csv.head(Limit):
+    for i in inpt_csv[j].head(Limit):
+        if i> Min_value:
+            print(f"{j}: ",i)
