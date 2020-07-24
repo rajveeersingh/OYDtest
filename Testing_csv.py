dd4c0281ff415aa17b01cc4a7972d8ceb21f2223
rajveersingh <rajveer.rajput.18.1996@gmail.com>
2020-07-24 12:39:24 +0530

    OYD files

--- Testing_csv.py
+++ Testing_csv.py
@@ -4,7 +4,6 @@ inpt_csv = pd.read_csv("/home/superadmin/DBDA/project/OYD/summary.csv")
 
 del inpt_csv['ID']
 
-# print(inpt_csv.head(10))
 
 Limit = int(input("Enter Limit value:- "))
 
@@ -13,4 +12,6 @@ Min_value = float(input("Enter min double value between 0 and 1:- "))
 for j in inpt_csv.head(Limit):
     for i in inpt_csv[j].head(Limit):
         if i> Min_value:
-            print(f"{j}: ",round(i,9))
+            result =pd.DataFrame((f"{j}: ",round(i,9)))
+            print(result)
+            result.to_csv("/home/superadmin/DBDA/project/OYD/result.csv",mode='a',index=False)
